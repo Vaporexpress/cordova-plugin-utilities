873aea4e5cc7d68f37ad2b490af1792887e6bd1b
Vaporexpress <Vaporexpress@users.noreply.github.com>
2017-07-29 19:59:04 +0200

    Added iOS function getMimeType
    Added iOS function getMimeType

--- src/ios/CDVUtilities.h
+++ src/ios/CDVUtilities.h
@@ -1,12 +1,17 @@
 #import <Cordova/CDVPlugin.h>
 
-@interface CDVUtilities : CDVPlugin {
+@interface CDVUtilities : CDVPlugin
+{
   // Member variables go here.
 }
 
 // https://stackoverflow.com/questions/26047694/detecting-and-intercepting-video-playback-in-uiwebview
 // https://github.com/joshkehn/JSMessageExample
+/* Get Html Source (Don´t work with sites under CloudFlare) */
 - (void)getHtmlSource:(CDVInvokedUrlCommand*)command;
+/* Get MimeType from URL */
+- (void)getMimeType:(CDVInvokedUrlCommand*)command;
+/* Open External URL (like Android Intent) */
 - (void)openURL:(CDVInvokedUrlCommand*)command;
 
 @end
--- src/ios/CDVUtilities.m
+++ src/ios/CDVUtilities.m
@@ -8,27 +8,104 @@
 
 @implementation CDVUtilities
 
+
+// http://codewithchris.com/tutorial-how-to-use-ios-nsurlconnection-by-example/
+// https://www.objc.io/issues/5-ios7/from-nsurlconnection-to-nsurlsession/
+
 - (void)getHtmlSource:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
-    NSDictionary* objectProperties = [self objectProperties];
+    NSString *urlarg = [command.arguments objectAtIndex:0];
+    if (urlarg != nil && [urlarg length] > 0) {
+        [self getCode:urlarg:command];
+    } else {
+        [self pluginResultado:urlarg:command:NO];
+    }
+}
+- (void)getMimeType:(CDVInvokedUrlCommand*)command
+{
+    NSString *urlarg = [command.arguments objectAtIndex:0];
+    NSString* MIMEType;
+    if (urlarg != nil && [urlarg length] > 0) {
+        NSURL *URL = [NSURL URLWithString:urlarg];
+        NSMutableURLRequest *newRequest = [NSMutableURLRequest requestWithURL:URL];
+        NSURLResponse *response=nil;
+        NSError *error=nil;
+        
+        [newRequest setValue:@"HEAD" forKey:@"HTTPMethod"];
+        [NSURLConnection sendSynchronousRequest:newRequest returningResponse:&response error:&error];
+        //Get MIME type from the response
+        MIMEType = [response MIMEType];
+    } else {
+        MIMEType = nil;
+    }
+    [self pluginResultado:MIMEType:command:YES];
+}
 
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK  messageAsDictionary:objectProperties];
+- (void)pluginResultado:(NSString *)result :(CDVInvokedUrlCommand*) command :(BOOL) estado
+{
+    CDVPluginResult* pluginResult = nil;
+    if (estado) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:result];
+    }
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
-
-- (NSDictionary*)objectProperties
+- (void)getCode:(NSString *)urlarg :(CDVInvokedUrlCommand*) command
 {
-    NSMutableDictionary* devProps = [NSMutableDictionary dictionaryWithCapacity:1];
-    [devProps setObject:[self getHtmlCode] forKey:@"result"];
-
-    NSDictionary* devReturn = [NSDictionary dictionaryWithDictionary:devProps];
-    return devReturn;
+    NSURL *URL = [NSURL URLWithString:urlarg];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    
+    [NSURLConnection
+     sendAsynchronousRequest:request
+     queue:[[NSOperationQueue alloc] init]
+     completionHandler:^(NSURLResponse *response,
+                         NSData *data,
+                         NSError *error)
+     {
+         
+         if ([data length] >0 && error == nil)
+         {
+             
+              /* Get the Source - No funciona con CoudFlare */
+             //NSString *newStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+             NSString *newStr = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+             //NSLog(@"responseDataConvers: %@", newStr);
+             [self pluginResultado:newStr:command:YES];
+             /* Get the MimeType */
+             //NSString* mimeType = [response MIMEType];
+             //[self pluginResultado:mimeType:command:YES];
+             
+         }
+         else if ([data length] == 0 && error == nil)
+         {
+             NSLog(@"Nothing was downloaded.");
+             [self pluginResultado:@"Nothing was downloaded.":command:NO];
+         }
+         else if (error != nil){
+             //NSLog(@"Error = %@", error);
+             [self pluginResultado:error.localizedDescription:command:NO];
+         }
+         
+     }];
 }
 
-- (NSString *)getHtmlCode
+- (void)getHtmlSource1:(CDVInvokedUrlCommand*)command
 {
-    return @"<div>Testing</div>";
+    CDVPluginResult* pluginResult = nil;
+    NSString *urlarg = [command.arguments objectAtIndex:0];
+    
+    if (urlarg != nil && [urlarg length] > 0) {
+        NSURL *url = [NSURL URLWithString:urlarg];
+        // Deprecated NSString *webData= [NSString stringWithContentsOfURL:url];
+        NSString *webData = [NSString stringWithContentsOfURL: url encoding:NSUTF8StringEncoding error:nil];
+        //NSLog(@"%@",webData);
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:webData];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)openURL:(CDVInvokedUrlCommand*)command
