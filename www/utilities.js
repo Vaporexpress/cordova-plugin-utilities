b44a2f32049aa3fcc6cc320a4f46422e5ba91bbe
Vaporexpress <Vaporexpress@users.noreply.github.com>
2017-07-29 21:58:02 +0200

    Vuelta atras
    Vuelta atras

--- www/utilities.js
+++ www/utilities.js
@@ -1,42 +1,13 @@
-var argscheck = require('cordova/argscheck'),
-    utils = require('cordova/utils'),
-    exec = require('cordova/exec');
+var exec = require('cordova/exec');
 
-
-var Utilities = function () {};
-
-/**
- * getHtmlSource
- *
- * @param {Function} successCallback The function to call when the heading data is available
- * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
- */
-Utilities.prototype.getHtmlSource = function(successCallback, errorCallback) {
-    argscheck.checkArgs('fF', 'Utilities.getHtmlSource', arguments);
-    exec(successCallback, errorCallback, "Utilities", "getHtmlSource", [url]);
+exports.getHtmlSource = function (url, success, error) {
+    exec(success || null, error || null, "Utilities", "getHtmlSource", [url]);
 };
 
-/**
- * getMimeType
- *
- * @param {Function} successCallback The function to call when the heading data is available
- * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
- */
-Utilities.prototype.getMimeType = function(successCallback, errorCallback) {
-    argscheck.checkArgs('fF', 'Utilities.getMimeType', arguments);
-    exec(successCallback, errorCallback, "Utilities", "getMimeType", [url]);
+exports.getMimeType = function (url, success, error) {
+    exec(success || null, error || null, "Utilities", "getMimeType", [url]);
 };
 
-/**
- * openURL
- *
- * @param {Function} successCallback The function to call when the heading data is available
- * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
- */
-Utilities.prototype.openURL = function(successCallback, errorCallback) {
-    argscheck.checkArgs('fF', 'Utilities.openURL', arguments);
-    exec(successCallback, errorCallback, "Utilities", "openURL", [url]);
+exports.openURL = function (url, success, error) {
+    exec(success || null, error || null, "Utilities", "openURL", [url]);
 };
-
-module.exports = Utilities;
-
