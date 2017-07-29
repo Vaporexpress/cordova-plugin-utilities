f18a840a9283c1aa6108892a9fda3aaac129c202
Vaporexpress <Vaporexpress@users.noreply.github.com>
2017-07-29 20:01:50 +0200

    Added iOS function getMimeType (now , only for iOS)
    Added iOS function getMimeType (now , only for iOS)

--- www/utilities.js
+++ www/utilities.js
@@ -1,9 +1,15 @@
+cordova.define("cordova-plugin-utilities.utilities", function(require, exports, module) {
 var exec = require('cordova/exec');
 
 exports.getHtmlSource = function (url, success, error) {
     exec(success || null, error || null, "Utilities", "getHtmlSource", [url]);
 };
 
+exports.getMimeType = function (url, success, error) {
+    exec(success || null, error || null, "Utilities", "getMimeType", [url]);
+};
+
 exports.openURL = function (url, success, error) {
     exec(success || null, error || null, "Utilities", "openURL", [url]);
 };
+});
